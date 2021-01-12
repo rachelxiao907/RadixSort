48e1c49675c42e22b54ae5028603baf96b3c626e
Rachel Xiao <rachelxiao907@gmail.com>
2021-01-12 12:09:09 -0500

    Created length method

--- Radix.java
+++ Radix.java
@@ -2,4 +2,8 @@ public class Radix {
   public static int nth(int n, int col) {
     return Math.abs(n) / (int)Math.pow(10, col) % 10;
   }
+  public static int length(int n) {
+    String s = Integer.toString(Math.abs(n));
+    return s.length();
+  }
 }
