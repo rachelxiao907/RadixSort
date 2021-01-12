d1ce9a3cbeae8e11be9ca430479964cb1e354457
Rachel Xiao <rachelxiao907@gmail.com>
2021-01-12 12:16:46 -0500

    Created merge

--- Radix.java
+++ Radix.java
@@ -2,8 +2,15 @@ public class Radix {
   public static int nth(int n, int col) {
     return Math.abs(n) / (int)Math.pow(10, col) % 10;
   }
+
   public static int length(int n) {
     String s = Integer.toString(Math.abs(n));
     return s.length();
+  }
+
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+    for (int i = 0; i < buckets.length; i++) {
+      original.extend(buckets[i]);
+    }
   }
 }
