d6db42aa8dd46fa5e48635a97df483efb3f4e381
Rachel Xiao <rachelxiao907@gmail.com>
2021-01-13 00:35:22 -0500

    I think I finished radixSortSimple

--- Radix.java
+++ Radix.java
@@ -8,9 +8,29 @@ public class Radix {
     return s.length();
   }
 
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
     for (int i = 0; i < buckets.length; i++) {
       original.extend(buckets[i]);
+    }
+  }
+
+  public static void radixSortSimple(SortableLinkedList data) {
+    int passes = 0;
+    for (int i = 0; i < data.size(); i++) {
+      passes = Math.max(length(data.get(i)), passes);
+    }
+    SortableLinkedList[] bucket = new SortableLinkedList[10];
+    for (int i = 0; i < 10; i++) {
+      bucket[i] = new SortableLinkedList(); //make 10 lists for digit's 10 different values
+    }
+    for (int i = 0; i < passes; i++) {  //i is ones column, tens column, etc
+      while(data.size() != 0) {
+        int value = data.get(0);
+        int digit = nth(value, i);  //find the bucket to put it in
+        bucket[digit].add(value);
+        data.remove(0);
+      }
+      merge(data, buckets);
     }
   }
 }
